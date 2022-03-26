4fd429b9933b6b783c99ff459922b6142836e1fa
Anton Sychevskij <antoshka336kartoshka@gmail.com>
2022-03-26 11:11:47 +0300

    Update

--- solutions.py
+++ solutions.py
@@ -1865,10 +1865,30 @@ class Solution:
         dfs(root)
         return all(path[i] < path[i+1] for i in range(len(path) - 1))
 
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        Given an array of integers nums which is sorted in ascending order, 
+        and an integer target, write a function to search target in nums. 
+        If target exists, then return its index. Otherwise, return -1.
+        You must write an algorithm with O(log n) runtime complexity.
+        """
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            print("mid=", nums[mid])
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return -1
+
+
+
 
 if __name__ == '__main__':
     '''
-
 
     '''
     s = Solution()
